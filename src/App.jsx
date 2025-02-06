e7dda208d40230b91aad3c680a7a3f77b6f390f1
Dariusz Okoński <darek200180@gmail.com>
2025-02-06 18:58:08 +0100

    add Pagination test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Pagination from './pagination/Pagination';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Pagination total={50} limit={10} currentPage={1} />
+    </div>
+  );
 };
 
 export default App;
