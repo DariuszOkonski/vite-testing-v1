36fd80825ee4bd5eb4edef3ab51aedd7e9471915
Dariusz Okoński <darek200180@gmail.com>
2025-02-06 18:30:14 +0100

    add ErrorMessage test

---
+++ src/errorMessage/ErrorMessage.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import ErrorMessage from './ErrorMessage';
+
+describe('ErrorMessage', () => {
+  it('renders default error state', () => {
+    render(<ErrorMessage />);
+    const messageContainer = screen.getByTestId('message-container');
+    expect(messageContainer).toHaveTextContent(/something went wrong/i);
+  });
+
+  it('render custom error state', () => {
+    render(<ErrorMessage message='Email is already taken' />);
+    const messageContainer = screen.getByTestId('message-container');
+    expect(messageContainer).toHaveTextContent(/email is already taken/i);
+  });
+});
