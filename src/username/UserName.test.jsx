9d2d9e3f889e64af1efc1763cb386da55fa7338d
Dariusz Okoński <darek200180@gmail.com>
2025-02-11 17:05:52 +0100

    add tests for username

--- src/username/UserName.test.jsx
+++ src/username/UserName.test.jsx
@@ -17,4 +17,42 @@ describe('UserName', () => {
     await user.click(button);
     expect(screen.getByTestId('username')).toHaveTextContent('bar');
   });
+
+  it('renders changed username with input', async () => {
+    const user = userEvent.setup();
+    render(<UserName />);
+
+    const input = screen.getByTestId('usernameInput');
+    await user.type(input, 'foo');
+
+    expect(screen.getByTestId('username')).toHaveTextContent('foo');
+  });
+});
+
+describe('UserName v2', () => {
+  it('should render a component', () => {
+    render(<UserName />);
+    const username = screen.getByTestId('username');
+    expect(username).toHaveTextContent('');
+  });
+
+  it('should render bar after clicking a button', async () => {
+    const user = userEvent.setup();
+    render(<UserName />);
+
+    const button = screen.getByTestId('button');
+    await user.click(button);
+
+    expect(screen.getByTestId('username')).toHaveTextContent('bar');
+  });
+
+  it('should render text changed in input', async () => {
+    const user = userEvent.setup();
+    render(<UserName />);
+
+    const input = screen.getByTestId('usernameInput');
+    await user.type(input, 'foo');
+
+    expect(screen.getByTestId('username')).toHaveTextContent('foo');
+  });
 });
