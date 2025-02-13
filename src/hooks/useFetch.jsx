c621ab27b6866abe510d1f00989038157b7e7b07
Dariusz Okoński <darek200180@gmail.com>
2025-02-13 17:23:14 +0100

    add useFetch hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useFetch = (url) => {
+  const baseUrl = 'http://localhost:3004';
+  const [response, setResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [options, setOptions] = useState({});
+  const token = localStorage.getItem('token');
+
+  const doFetch = (options = {}) => {
+    setOptions(options);
+    setIsLoading(true);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!isLoading) {
+          return;
+        }
+        const requestOptions = {
+          ...options,
+          ...{
+            headers: {
+              authorization: token ? `Token ${token}` : '',
+            },
+          },
+        };
+        const res = await axios.request(baseUrl + url, requestOptions);
+        setResponse(res.data);
+      } catch (err) {
+        setError(err.response.data);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [isLoading, options, token, url]);
+
+  return [{ response, isLoading, error }, doFetch];
+};
+
+export default useFetch;
