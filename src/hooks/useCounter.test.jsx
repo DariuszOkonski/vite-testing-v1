7f99f1a015b93a0954b4050b997660ce706bab5a
Dariusz Okoński <darek200180@gmail.com>
2025-02-18 18:07:13 +0100

    add tests for simple hook

--- src/hooks/useCounter.test.jsx
+++ src/hooks/useCounter.test.jsx
@@ -2,7 +2,27 @@ import { renderHook, act } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import useCounter from './useCounter';
 
-describe('useCounter', () => {
+describe('useCounter V2', () => {
+  it('should render initial count', () => {
+    const { result } = renderHook(() => useCounter());
+    expect(result.current.count).toEqual(0);
+  });
+
+  it('should render with given number', () => {
+    const { result } = renderHook(() => useCounter(10));
+    expect(result.current.count).toEqual(10);
+  });
+
+  it('should increment the count', () => {
+    const { result } = renderHook(() => useCounter(0));
+    act(() => {
+      result.current.increment();
+    });
+    expect(result.current.count).toEqual(1);
+  });
+});
+
+describe.skip('useCounter V1', () => {
   it('should render initial count', () => {
     const { result } = renderHook(() => useCounter());
     expect(result.current.count).toEqual(0);
