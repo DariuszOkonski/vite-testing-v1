431d4fe1702bc7f4557e395b9e00ce40f053e60f
Dariusz Okoński <darek200180@gmail.com>
2025-02-20 17:06:02 +0100

    add errormesage test

--- src/errorMessage/ErrorMessage.test.jsx
+++ src/errorMessage/ErrorMessage.test.jsx
@@ -5,13 +5,21 @@ import ErrorMessage from './ErrorMessage';
 describe('ErrorMessage', () => {
   it('renders default error state', () => {
     render(<ErrorMessage />);
-    const messageContainer = screen.getByTestId('message-container');
-    expect(messageContainer).toHaveTextContent(/something went wrong/i);
-  });
-
-  it('render custom error state', () => {
-    render(<ErrorMessage message='Email is already taken' />);
-    const messageContainer = screen.getByTestId('message-container');
-    expect(messageContainer).toHaveTextContent(/email is already taken/i);
+    const result = screen.getByTestId('message-container');
+    expect(result.textContent).toMatch(/went wrong/i);
   });
 });
+
+// describe('ErrorMessage', () => {
+//   it('renders default error state', () => {
+//     render(<ErrorMessage />);
+//     const messageContainer = screen.getByTestId('message-container');
+//     expect(messageContainer).toHaveTextContent(/something went wrong/i);
+//   });
+
+//   it('render custom error state', () => {
+//     render(<ErrorMessage message='Email is already taken' />);
+//     const messageContainer = screen.getByTestId('message-container');
+//     expect(messageContainer).toHaveTextContent(/email is already taken/i);
+//   });
+// });
