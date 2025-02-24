f38b8256bcb44c081afe989dba101dc7f13a835b
Dariusz Okoński <darek200180@gmail.com>
2025-02-24 17:30:23 +0100

    testing error message

--- src/errorMessage/ErrorMessage.test.jsx
+++ src/errorMessage/ErrorMessage.test.jsx
@@ -17,15 +17,3 @@ describe('ErrorMessage', () => {
     );
   });
 });
-//   it('renders default error state', () => {
-//     render(<ErrorMessage />);
-//     const messageContainer = screen.getByTestId('message-container');
-//     expect(messageContainer).toHaveTextContent(/something went wrong/i);
-//   });
-
-//   it('render custom error state', () => {
-//     render(<ErrorMessage message='Email is already taken' />);
-//     const messageContainer = screen.getByTestId('message-container');
-//     expect(messageContainer).toHaveTextContent(/email is already taken/i);
-//   });
-// });
