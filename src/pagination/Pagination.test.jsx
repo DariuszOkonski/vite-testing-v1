538fa3fa3a281b5b0bbcd4c400dfc4caf10cefdc
Dariusz Okoński <darek200180@gmail.com>
2025-02-24 17:55:11 +0100

    add tests for pagination

--- src/pagination/Pagination.test.jsx
+++ src/pagination/Pagination.test.jsx
@@ -4,9 +4,36 @@ import Pagination from './Pagination';
 import userEvent from '@testing-library/user-event';
 // import * as utils from '../utils';
 
-describe('Pagination', () => {});
+describe('Pagination', () => {
+  it('renders correct pagination', () => {
+    render(<Pagination total={50} limit={10} currentPage={1} />);
 
-// describe('Pagination', () => {
+    const pageContainers = screen.getAllByTestId('page-container');
+
+    expect(pageContainers.length).toBe(5);
+    expect(pageContainers).toHaveLength(5);
+    expect(pageContainers[0]).toHaveTextContent(1);
+  });
+
+  it('should emit clicked page', async () => {
+    const user = userEvent.setup();
+    const handleClick = vi.fn();
+    render(
+      <Pagination
+        total={50}
+        limit={10}
+        currentPage={1}
+        selectPage={handleClick}
+      />
+    );
+
+    const pageContainers = screen.getAllByTestId('page-container');
+    await user.click(pageContainers[0]);
+
+    expect(handleClick).toHaveBeenCalledOnce();
+    expect(handleClick).toHaveBeenCalledWith(1);
+  });
+});
 //   it('renders correct pagination', () => {
 //     render(<Pagination total={50} limit={10} currentPage={1} />);
 //     const pageContainers = screen.getAllByTestId('page-container');
