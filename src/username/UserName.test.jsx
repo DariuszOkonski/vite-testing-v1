6e439d2f398efe8469d214351ce592db1a80db85
Dariusz Okoński <darek200180@gmail.com>
2025-02-25 20:26:47 +0100

    add type test to username

--- src/username/UserName.test.jsx
+++ src/username/UserName.test.jsx
@@ -18,6 +18,16 @@ describe('UserName', () => {
 
     expect(screen.getByTestId('username')).toHaveTextContent('bar');
   });
+
+  it('renders changed username with input', async () => {
+    const user = userEvent.setup();
+    render(<UserName />);
+
+    const input = screen.getByTestId('usernameInput');
+    await user.type(input, 'foo');
+
+    expect(screen.getByTestId('username')).toHaveTextContent('foo');
+  });
 });
 
 // describe('UserName', () => {
