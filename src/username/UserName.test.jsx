efdb9e5f7f6079c6eac902f6f28efff0339af5ae
Dariusz Okoński <darek200180@gmail.com>
2025-02-27 10:10:53 +0100

    working on tests

--- src/username/UserName.test.jsx
+++ src/username/UserName.test.jsx
@@ -29,29 +29,3 @@ describe('UserName', () => {
     expect(screen.getByTestId('username')).toHaveTextContent('foo');
   });
 });
-
-// describe('UserName', () => {
-//   it('renders default empty text', () => {
-//     render(<UserName />);
-//     expect(screen.getByTestId('username')).toHaveTextContent('');
-//   });
-
-//   it('renders changed username with button', async () => {
-//     const user = userEvent.setup();
-//     render(<UserName />);
-
-//     const button = screen.getByTestId('button');
-//     await user.click(button);
-//     expect(screen.getByTestId('username')).toHaveTextContent('bar');
-//   });
-
-//   it('renders changed username with input', async () => {
-//     const user = userEvent.setup();
-//     render(<UserName />);
-
-//     const input = screen.getByTestId('usernameInput');
-//     await user.type(input, 'foo');
-
-//     expect(screen.getByTestId('username')).toHaveTextContent('foo');
-//   });
-// });
