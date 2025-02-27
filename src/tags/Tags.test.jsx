2e5861359ab0c5681cec34a5605b0c7e8f8f44ea
Dariusz Okoński <darek200180@gmail.com>
2025-02-27 10:41:52 +0100

    add test for tags

--- src/tags/Tags.test.jsx
+++ src/tags/Tags.test.jsx
@@ -14,6 +14,7 @@ import { setupServer } from 'msw/node';
 import { http, HttpResponse } from 'msw';
 
 describe('Tags', () => {
+  // TODO: its only for V1
   const server = setupServer(
     http.get('http://localhost:3004/tags', () => {
       return HttpResponse.json([{ id: 1, name: 'bar' }]);
@@ -28,30 +29,22 @@ describe('Tags', () => {
     render(<Tags />);
     const tags = await screen.findAllByTestId('tag');
     expect(tags).toHaveLength(1);
+    expect(tags[0]).toHaveTextContent('bar');
+  });
+
+  it('renders tags V2', async () => {
+    const mockResponse = {
+      data: [
+        { id: 1, name: 'bar' },
+        { id: 2, name: 'foo' },
+      ],
+    };
+    vi.spyOn(axios, 'get').mockResolvedValue(mockResponse);
+
+    render(<Tags />);
+    const tags = await screen.findAllByTestId('tag');
+
+    expect(tags).toHaveLength(2);
+    expect(tags[1]).toHaveTextContent('foo');
   });
 });
-
-// describe('Tags', () => {
-//   // const server = setupServer(
-//   //   http.get('http://localhost:3004/tags', () => {
-//   //     return HttpResponse.json([{ id: '1', name: 'bar' }]);
-//   //   })
-//   // );
-
-//   // beforeAll(() => server.listen());
-//   // afterAll(() => server.close());
-//   // afterEach(() => server.resetHandlers());
-
-//   it('renders tags', async () => {
-//     const mockResponse = {
-//       data: [{ id: '1', name: 'bar' }],
-//     };
-//     vi.spyOn(axios, 'get').mockResolvedValue(mockResponse);
-
-//     render(<Tags />);
-//     const tags = await screen.findAllByTestId('tag');
-
-//     expect(tags).toHaveLength(1);
-//     expect(tags[0]).toHaveTextContent('bar');
-//   });
-// });
