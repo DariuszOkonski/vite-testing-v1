acd8a5d73b425987cc70ad8c3f4963e39ceb176a
Dariusz Okoński <darek200180@gmail.com>
2025-02-27 10:36:59 +0100

    add test for tags

--- src/tags/Tags.test.jsx
+++ src/tags/Tags.test.jsx
@@ -1,29 +1,57 @@
 import { render, screen } from '@testing-library/react';
 import axios from 'axios';
-import { describe, expect, it, vi } from 'vitest';
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from 'vitest';
 import Tags from './Tags';
+import { setupServer } from 'msw/node';
+import { http, HttpResponse } from 'msw';
 
 describe('Tags', () => {
-  // const server = setupServer(
-  //   http.get('http://localhost:3004/tags', () => {
-  //     return HttpResponse.json([{ id: '1', name: 'bar' }]);
-  //   })
-  // );
+  const server = setupServer(
+    http.get('http://localhost:3004/tags', () => {
+      return HttpResponse.json([{ id: 1, name: 'bar' }]);
+    })
+  );
 
-  // beforeAll(() => server.listen());
-  // afterAll(() => server.close());
-  // afterEach(() => server.resetHandlers());
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+  afterEach(() => server.resetHandlers());
 
-  it('renders tags', async () => {
-    const mockResponse = {
-      data: [{ id: '1', name: 'bar' }],
-    };
-    vi.spyOn(axios, 'get').mockResolvedValue(mockResponse);
-
+  it('renders tags V1', async () => {
     render(<Tags />);
     const tags = await screen.findAllByTestId('tag');
-
     expect(tags).toHaveLength(1);
-    expect(tags[0]).toHaveTextContent('bar');
   });
 });
+
+// describe('Tags', () => {
+//   // const server = setupServer(
+//   //   http.get('http://localhost:3004/tags', () => {
+//   //     return HttpResponse.json([{ id: '1', name: 'bar' }]);
+//   //   })
+//   // );
+
+//   // beforeAll(() => server.listen());
+//   // afterAll(() => server.close());
+//   // afterEach(() => server.resetHandlers());
+
+//   it('renders tags', async () => {
+//     const mockResponse = {
+//       data: [{ id: '1', name: 'bar' }],
+//     };
+//     vi.spyOn(axios, 'get').mockResolvedValue(mockResponse);
+
+//     render(<Tags />);
+//     const tags = await screen.findAllByTestId('tag');
+
+//     expect(tags).toHaveLength(1);
+//     expect(tags[0]).toHaveTextContent('bar');
+//   });
+// });
