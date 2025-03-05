03e2957d2399708761adaa18016feca61aeb6821
Dariusz Okoński <darek200180@gmail.com>
2025-03-05 20:19:03 +0100

    testing userName

--- src/username/UserName.test.jsx
+++ src/username/UserName.test.jsx
@@ -3,29 +3,29 @@ import { describe, expect, it } from 'vitest';
 import UserName from './UserName';
 import userEvent from '@testing-library/user-event';
 
-describe('UserName', () => {
-  it('renders default empty text', () => {
-    render(<UserName />);
-    expect(screen.getByTestId('username')).toHaveTextContent('');
-  });
+// describe('UserName', () => {
+//   it('renders default empty text', () => {
+//     render(<UserName />);
+//     expect(screen.getByTestId('username')).toHaveTextContent('');
+//   });
 
-  it('renders changed username with button', async () => {
-    const user = userEvent.setup();
-    render(<UserName />);
+//   it('renders changed username with button', async () => {
+//     const user = userEvent.setup();
+//     render(<UserName />);
 
-    const button = screen.getByTestId('button');
-    await user.click(button);
+//     const button = screen.getByTestId('button');
+//     await user.click(button);
 
-    expect(screen.getByTestId('username')).toHaveTextContent('bar');
-  });
+//     expect(screen.getByTestId('username')).toHaveTextContent('bar');
+//   });
 
-  it('renders changed username with input', async () => {
-    const user = userEvent.setup();
-    render(<UserName />);
+//   it('renders changed username with input', async () => {
+//     const user = userEvent.setup();
+//     render(<UserName />);
 
-    const input = screen.getByTestId('usernameInput');
-    await user.type(input, 'foo');
+//     const input = screen.getByTestId('usernameInput');
+//     await user.type(input, 'foo');
 
-    expect(screen.getByTestId('username')).toHaveTextContent('foo');
-  });
-});
+//     expect(screen.getByTestId('username')).toHaveTextContent('foo');
+//   });
+// });
