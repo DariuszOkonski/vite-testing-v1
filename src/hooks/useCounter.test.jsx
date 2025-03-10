0c7d44832a04960fe4d802dbe168ef14c1a18802
Dariusz Okoński <darek200180@gmail.com>
2025-03-10 16:07:25 +0100

    testing useCounter

--- src/hooks/useCounter.test.jsx
+++ src/hooks/useCounter.test.jsx
@@ -2,42 +2,42 @@ import { renderHook, act } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import useCounter from './useCounter';
 
-describe('useCounter V2', () => {
-  it('should render initial count', () => {
-    const { result } = renderHook(() => useCounter());
-    expect(result.current.count).toEqual(0);
-  });
+// describe('useCounter V2', () => {
+//   it('should render initial count', () => {
+//     const { result } = renderHook(() => useCounter());
+//     expect(result.current.count).toEqual(0);
+//   });
 
-  it('should render with given number', () => {
-    const { result } = renderHook(() => useCounter(10));
-    expect(result.current.count).toEqual(10);
-  });
+//   it('should render with given number', () => {
+//     const { result } = renderHook(() => useCounter(10));
+//     expect(result.current.count).toEqual(10);
+//   });
 
-  it('should increment the count', () => {
-    const { result } = renderHook(() => useCounter(0));
-    act(() => {
-      result.current.increment();
-    });
-    expect(result.current.count).toEqual(1);
-  });
-});
+//   it('should increment the count', () => {
+//     const { result } = renderHook(() => useCounter(0));
+//     act(() => {
+//       result.current.increment();
+//     });
+//     expect(result.current.count).toEqual(1);
+//   });
+// });
 
-describe.skip('useCounter V1', () => {
-  it('should render initial count', () => {
-    const { result } = renderHook(() => useCounter());
-    expect(result.current.count).toEqual(0);
-  });
+// describe.skip('useCounter V1', () => {
+//   it('should render initial count', () => {
+//     const { result } = renderHook(() => useCounter());
+//     expect(result.current.count).toEqual(0);
+//   });
 
-  it('should render changed initial value', () => {
-    const { result } = renderHook(() => useCounter(10));
-    expect(result.current.count).toEqual(10);
-  });
+//   it('should render changed initial value', () => {
+//     const { result } = renderHook(() => useCounter(10));
+//     expect(result.current.count).toEqual(10);
+//   });
 
-  it('should increment the count', () => {
-    const { result } = renderHook(() => useCounter());
-    act(() => {
-      result.current.increment();
-    });
-    expect(result.current.count).toEqual(1);
-  });
-});
+//   it('should increment the count', () => {
+//     const { result } = renderHook(() => useCounter());
+//     act(() => {
+//       result.current.increment();
+//     });
+//     expect(result.current.count).toEqual(1);
+//   });
+// });
