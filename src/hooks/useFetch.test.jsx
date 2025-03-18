a578c7c7cc254d6e3d0b621a651cf49983f6d294
Dariusz Okoński <darek200180@gmail.com>
2025-03-18 18:59:10 +0100

    add test for useFetch

--- src/hooks/useFetch.test.jsx
+++ src/hooks/useFetch.test.jsx
@@ -4,7 +4,49 @@ import useFetch from './useFetch';
 import axios from 'axios';
 
 describe('useFetch V2', () => {
-  it('should', () => {});
+  it('should render initial values', () => {
+    const { result } = renderHook(() => useFetch('/todos'));
+    const [{ error, isLoading, response }, doFetch] = result.current;
+
+    expect(error).toEqual(null);
+    expect(isLoading).toEqual(false);
+    expect(response).toEqual(null);
+    expect(doFetch).toBeDefined();
+  });
+
+  it('should render success values after fetch', async () => {
+    const mockResponse = {
+      data: [{ id: '1', text: 'foo', isCompleted: false }],
+    };
+    vi.spyOn(axios, 'request').mockResolvedValue(mockResponse);
+    const { result } = renderHook(() => useFetch('/todos'));
+
+    await act(() => {
+      result.current[1]();
+    });
+
+    const [{ error, isLoading, response }] = result.current;
+    expect(error).toEqual(null);
+    expect(isLoading).toEqual(false);
+    expect(response).toEqual(mockResponse.data);
+  });
+
+  it('should render error values after fetch', async () => {
+    const mockResponse = {
+      response: { data: 'Server error' },
+    };
+    vi.spyOn(axios, 'request').mockRejectedValue(mockResponse);
+    const { result } = renderHook(() => useFetch('/todos'));
+
+    await act(() => {
+      result.current[1]();
+    });
+
+    const [{ error, isLoading, response }] = result.current;
+    expect(error).toEqual('Server error');
+    expect(isLoading).toEqual(false);
+    expect(response).toEqual(null);
+  });
 });
 
 describe('useFetch', () => {
